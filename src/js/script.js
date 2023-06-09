dd3cc75dfeec1978c2bb0b28d0d75f3bad91422a
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-06-09 13:33:04 +0200

    Add function initActions

--- src/js/script.js
+++ src/js/script.js
@@ -10,21 +10,52 @@
     containerOf: {
       books: '.books-list',
     },
+    book: {
+      image: '.book__image',
+    },
   };
+
+  const classNames = {
+    books: {
+      favorite:'favorite',
+    },
+  };
+
+  const templatesBooks = Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML);
+  const booksContainer = document.querySelector(select.containerOf.books);
+
+  const favoriteBooks = [];
+
   /**
    * Function for rendering books on the page.
    * Books are generated from templates.
    * The data for the templates comes from data.js
    */
   const renderBooks = function(){
-    const templates = Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML);
     for(const element of dataSource.books){
-      const generatedHTML = templates(element);
+      const generatedHTML = templatesBooks(element);
       const elementDOM = utils.createDOMFromHTML(generatedHTML);
-      const booksContainer = document.querySelector(select.containerOf.books);
       booksContainer.appendChild(elementDOM);
     }
   };
 
+  const initActions = function(){
+    console.log(booksContainer);
+    const books = booksContainer.getElementsByTagName('li');
+    for(let i = 0; i < books.length; ++i){
+      console.log(books[i]);
+      books[i].addEventListener('dblclick', function(event) {
+        event.preventDefault();
+        const bookId = event.target.offsetParent.getAttribute('data-id');
+        event.target.offsetParent.classList.add(classNames.books.favorite);
+        favoriteBooks.push(bookId);
+        console.log(favoriteBooks);
+      });
+    }
+      
+  };
+
   renderBooks();
+  initActions();
+  console.log(favoriteBooks);
 }
